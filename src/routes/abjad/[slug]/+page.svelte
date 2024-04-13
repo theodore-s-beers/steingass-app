1b2183194376b3b6b3be131caa3c5407c4ab2d2c
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2024-04-14 00:39:56 +0200

    Add abjad route

---
+++ src/routes/abjad/[slug]/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { afterNavigate } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { type AbjadEntry } from '$lib/utils';
+
+	$: abjadValue = $page.params.slug;
+	$: title = `Steingass – Abjad value ${abjadValue}`;
+	let loading = true;
+	let entries: AbjadEntry[] = [];
+	$: count = entries.length;
+
+	async function fetchAbjad(value: string): Promise<AbjadEntry[]> {
+		try {
+			const res = await fetch('/api/abjad?' + new URLSearchParams({ value }));
+			if (!res.ok) {
+				throw new Error(`Failed query: ${res.status}`);
+			}
+
+			const data: AbjadEntry[] = await res.json();
+			return data;
+		} catch (err) {
+			console.error(err);
+			return [];
+		}
+	}
+
+	afterNavigate(async () => {
+		loading = true;
+		const data = await fetchAbjad(abjadValue);
+		entries = data;
+		loading = false;
+	});
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<div class="mb-4 flex justify-between">
+	<a href="/" class="text-blue-700 hover:underline">Home</a>
+</div>
+
+<h1 class="mb-5 text-4xl">{title}</h1>
+
+{#if loading}
+	<p>Loading…</p>
+{:else}
+	<p><span class="font-semibold">Entries:</span> {count}</p>
+{/if}
+
+<hr class="my-4 border border-dashed border-gray-400" />
+
+<ul>
+	{#each entries as entry}
+		<li>
+			<a href={'/entry/' + entry.id} class="text-blue-700 hover:underline">{entry.id}</a>:
+			<span class="font-mix">{entry.headword_persian}</span>
+		</li>
+	{/each}
+</ul>
